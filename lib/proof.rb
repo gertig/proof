01dce501a2ea545aeac732053d5b0ff8016fed54
Andrew Gertig <andrew.gertig@gmail.com>
2021-04-22 09:26:52 -0400

    Fix for autoload issue in Rails 6 in TEST env
    https://github.com/rails/rails/issues/36546
    
    https://github.com/Vasfed/wicked_pdf/blob/cd1c92f156ec182b4299366340a2903447a4d153/lib/wicked_pdf/railtie.rb

--- lib/proof.rb
+++ lib/proof.rb
@@ -3,6 +3,21 @@ require 'proof/proof_actions.rb'
 require 'proof/require_proof.rb'
 
 module Proof
-  ActionController::Base.send :include, Proof::ProofActions
-  ActionController::Base.send :include, Proof::RequireProof
+#    ActiveSupport.on_load(:action_controller) do
+#     if ActionController::Base.respond_to?(:prepend) &&
+#        Object.method(:new).respond_to?(:super_method)
+#       ActionController::Base.send :prepend, PdfHelper
+#     else
+#       ActionController::Base.send :include, PdfHelper
+#     end
+#     ActionView::Base.send :include, WickedPdfHelper::Assets
+#   end
+  
+  ActiveSupport.on_load(:action_controller) do
+    ActionController::Base.send :include, Proof::ProofActions
+    ActionController::Base.send :include, Proof::RequireProof
+  end
+  
+#   ActionController::Base.send :include, Proof::ProofActions
+#   ActionController::Base.send :include, Proof::RequireProof
 end
